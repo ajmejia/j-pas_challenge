5892bfb3b3b334cd2f62609845d9e7562115f45f
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2017-03-31 10:57:44 -0400

    fixing run scripts

--- mock-jpas.sh
+++ mock-jpas.sh
@@ -4,9 +4,9 @@ setenv  DYNBAS     $PWD/DynBaS
 setenv  DB_MODELS  $PWD/models
 setenv  DB_FILTERS $PWD/filters/FILTERBIN.RES
 
-foreach file ( `cat $1` )
-  if ( ! -e `echo "outs/$2/dynbasfit_$file" | sed "s/.txt/.log/"` ) then
+foreach file ( `cat $1/$2` )
+  if ( ! -e `echo "outs/$1/dynbasfit_$file" | sed "s/.txt/.log/"` ) then
     echo "fitting SED in $file"
-    $DYNBAS/bin/dynbas --Zsun=0.02 --models!=Z0.0000 --output-dir="outs/$2" -v --L-passband=17 --passbands=1,...,59 "$2/$file"
+    $DYNBAS/bin/dynbas --Zsun=0.02 --models!=Z0.0000 --output-dir="outs/$1" -v --L-passband=17 --passbands=1,...,59 "$1/$file"
   endif
 end
